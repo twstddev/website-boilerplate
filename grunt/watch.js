428c023e55b2efd6e302714387a9c297a3127eea
twstddev <twstd.dev@gmail.com>
2014-03-13 21:20:51 +0000

    Add assemble watching

--- grunt/watch.js
+++ grunt/watch.js
@@ -1,4 +1,16 @@
 module.exports = {
+	options : {
+		debounceDelay : 200
+	},
+
+	assemble : {
+		files : [
+			"<%= src %>/templates/**/*.hbs",
+			"<%= src %>/data/**/*.json"
+		],
+		tasks : [ "assemble" ]
+	},
+
 	development : {
 		files : "<%= build %>/**/*.*",
 		options : {
